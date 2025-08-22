b178c7ab5e9767b847b25cf49c80a4aff504cbc8
Manav Gandhi <gandhimanav91@gmail.com>
2025-08-22 12:48:00 +0530

    currency convter app final v1.0

--- currency convter/currencyconvter/src/App.jsx
+++ currency convter/currencyconvter/src/App.jsx
@@ -1,33 +1,74 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Input from './components/Input'
 import "./index.css"
 import useCurrencyinfo from "./Hooks/Usecurrencyinfo.hook.js"
 function App() {
-  const [ConvertedAmount, setConvertedAmount] = useState(0)
-  const [FromAmount,setFromAmount]=useState(0)
-  const [ToAmount,setToAmount]=useState(0)
-  const [FromCurrency,setFromCurrency]=useState('inr')
-  const [ToCurrency,setToCurrency]=useState('usd')
+  
+  const [FromAmount, setFromAmount] = useState(0)
+  const [ToAmount, setToAmount] = useState(0)
+  const [FromCurrency, setFromCurrency] = useState('inr')
+  const [ToCurrency, setToCurrency] = useState('usd')
 
-  let info=useCurrencyinfo(FromCurrency)
+  let info = useCurrencyinfo(FromCurrency)
 
-  let option=Object.keys(info)
+  let option = Object.keys(info)
+
+  function calculate(ToCurrency) {
+    let result=FromAmount* info[ToCurrency]
+    
+    setToAmount(result.toFixed(2))
+  }
+
+
+  function swap(){
+    setFromAmount(ToAmount)
+    setToAmount(FromAmount)
+    setFromCurrency(ToCurrency)
+    setToCurrency(FromCurrency)
+  }
   return (
-    <>
-      <div className='flex flex-col'>
 
-        <Input option={option} FromCurrency={FromCurrency} ToCurrency={ToCurrency} setFromCurrency={setFromCurrency} setToCurrency={setToCurrency} />
+    <div className='min-w-[300px]  w-[400px] flex flex-col justify-center items-center h-[400px] mx-3'>
 
-        <div className='w-full flex justify-center '>
-          <button className='bg-blue-600 text-white font-semibold px-3 py-1 rounded-md'>Sawp</button>
-        </div>
+      <Input option={option} FromAmount={FromAmount} FromCurrency={FromCurrency} ToCurrency={ToCurrency} setFromCurrency={setFromCurrency} setToCurrency={setToCurrency}
 
-        <Input option={option} label='to' inputdesabeld="true" FromCurrency={FromCurrency} ToCurrency={ToCurrency} setFromCurrency={setFromCurrency} setToCurrency={setToCurrency}  />
-        <div>
-          
-        </div>
+        setFromAmount={setFromAmount}
+      />
+
+      <div className='w-full flex justify-center my-4'>
+        <button
+          onClick={
+            swap
+          }
+          className='bg-gradient-to-r from-blue-700 via-blue-500 to-blue-400 text-white font-semibold px-5 py-2 rounded-lg shadow-md transition-all duration-300 hover:scale-105 hover:from-blue-500 hover:to-blue-700 hover:shadow-lg'
+        >
+          Swap
+        </button>
       </div>
-    </>
+
+      <Input
+        option={option}
+        label='to'
+        inputdesabeld="true"
+        FromCurrency={FromCurrency}
+        ToCurrency={ToCurrency}
+        setFromCurrency={setFromCurrency}
+        setToCurrency={setToCurrency}
+        ToAmount={ToAmount}
+      />
+
+      <div className='w-full flex justify-center my-4'>
+        <button
+          onClick={(e) => {
+            calculate(ToCurrency)
+          }}
+          className='bg-gradient-to-r from-blue-600 via-blue-500 to-blue-400 font-semibold text-white min-w-[300px] w-[400px] h-[40px] rounded-lg shadow-md transition-all duration-300 hover:scale-105 hover:from-blue-400 hover:to-blue-600 hover:shadow-lg'
+        >
+          {FromCurrency} to {ToCurrency}
+        </button>
+      </div>
+    </div>
+
   )
 }
 
--- currency convter/currencyconvter/src/components/Input.jsx
+++ currency convter/currencyconvter/src/components/Input.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../index.css"
 import { useId } from "react";
 function Input(
@@ -9,68 +9,56 @@ function Input(
         setFromAmount,
         setFromCurrency,
         FromCurrency,
+        FromAmount,
 
 
         setToCurrency,
         ToCurrency,
+        ToAmount,
         inputdesabeld = false
     }
 ) {
 
-
     return (
-        <>
-            <div className="flex min-w-[300px] flex-col gap-3 w-[400px] h-[100px] bg-white p-4 rounded-lg shadow-md m-2">
-                <label className="text-gray-500" htmlFor={label}>
-                    {label}
-                    <p className="inline ml-2 text-[#333230] font-semibold">
+        <div className="flex min-w-[300px] flex-col gap-3 w-full h-[100px] bg-gradient-to-r from-white/70 via-white/90 to-blue-100 p-4 rounded-xl shadow-lg m-2 border border-blue-200 backdrop-blur-md">
+            <label className="text-blue-700 font-semibold" htmlFor={label}>
+                {label}
+                <p className="inline ml-2 text-blue-900 font-bold tracking-wide">
+                    {label.toLowerCase() == "from" ? FromCurrency : ToCurrency}
+                </p>
+            </label>
+            <div className="w-full  flex justify-between items-center gap-2">
+                <input
 
-                        {label.toLowerCase() == "from" ? FromCurrency : ToCurrency}
-                    </p>
-                </label>
-                <div className="w-full  flex justify-between">
-
-                    <input
-
-                        disabled={inputdesabeld == "true" && true}
-
-                        onChange={(e) => {
-                            setFromAmount && setFromAmount(e.target.value);
-
-                        }}
-                        className="outline-none text-center customshadow  w-[70%] rounded-sm" id={label} type="number" />
-                    <select onChange={(e) => {
-
-                        if(label.toLowerCase() == "from")  setFromCurrency && setFromCurrency(e.target.value)
-
-
-                        if(label.toLowerCase() == "to")    setToCurrency && setToCurrency(e.target.value)
-
-
-
-
-                    }} className="customshadow w-[25%] text-center  rounded-sm" name="select" id="select">
-
-                        {
-
-
-                            option.map((opt) => (
-
-                                <option key={opt} value={opt}>
-                                    {opt}
-                                </option>
-
-
-                            ))
-                        }
-
-
-
-                    </select>
-                </div>
-
+                    value={label.toLocaleLowerCase() == "from" ? FromAmount : ToAmount}
+                    min={0}
+                    disabled={inputdesabeld == "true" && true}
+                    onChange={(e) => {
+                        setFromAmount && setFromAmount(e.target.value);
+                    }}
+                    className="outline-none  text-blue-900 bg-white/80 customshadow w-[70%] h-[30px] rounded-md px-3 py-2 border border-blue-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 transition-all duration-200 text-center"
+                    id={label}
+                    type="number"
+                />
+                <select
+                    value={label.toLocaleLowerCase() == "from" ? FromCurrency : ToCurrency}
+                    onChange={(e) => {
+                        if (label.toLowerCase() == "from") setFromCurrency && setFromCurrency(e.target.value)
+                        if (label.toLowerCase() == "to") setToCurrency && setToCurrency(e.target.value)
+                        setSelected(e.target.value)
+                    }}
+                    className="customshadow w-[25%] h-[30px] text-blue-900 bg-white/80 rounded-md  border border-blue-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 transition-all duration-200 text-center"
+                    name="select"
+                    id="select"
+                >
+                    {option.map((opt) => (
+                        <option key={opt} value={opt}>
+                            {opt}
+                        </option>
+                    ))}
+                </select>
             </div>
-        </>
+        </div>
     )
 }
 
